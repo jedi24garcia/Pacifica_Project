f42c3d9a3774112374b18d27c7cbda26508ed370
jedi24garcia <jedi24garcia@gmail.com>
2025-03-10 11:00:07 +1300

    added more eatery sections, might not include bookings as this is for learning and research only website

--- dine/models.py
+++ dine/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Dine(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    features = models.TextField(max_length=100, default="No features available")
 
     def __str__(self):
         return self.name

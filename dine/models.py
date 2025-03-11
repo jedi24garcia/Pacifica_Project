ef8baedcb3de25478f7f841ddb3fd2e70b0d3099
jedi24garcia <jedi24garcia@gmail.com>
2025-03-11 18:00:20 +1300

    images function for each pages finally working

--- dine/models.py
+++ dine/models.py
@@ -5,6 +5,7 @@ class Dine(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     features = models.TextField(max_length=100, default="No features available")
+    image = models.ImageField(upload_to='dine_images/', null=True, blank=True)
 
     def __str__(self):
         return self.name

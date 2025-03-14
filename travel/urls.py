b2fcdcdfe219e666c8315b14a2de90bb64190975
jedi24garcia <jedi24garcia@gmail.com>
2025-03-14 15:33:39 +1300

    modifed footer and added pages that correspond to the links inside footer

--- travel/urls.py
+++ travel/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
   path('', views.base, name='homepage'),
+  path('about/', views.AboutPage, name='about'),
+  path('work/', views.WorkWithUsPage, name='work'),
+  path('media/', views.MediaPage, name='media'),
+  path('resources/', views.ResourcesPage, name='resources'),
 ]
--- travel/views.py
+++ travel/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def base(request):
   return render(request, 'homepage.html')
+
+def AboutPage(request):
+  return render(request, 'aboutpage.html')
+
+def WorkWithUsPage(request):
+  return render(request, 'workpage.html')
+
+def MediaPage(request):
+  return render(request, 'mediapage.html')
+
+def ResourcesPage(request):
+  return render(request, 'resourcespage.html')
